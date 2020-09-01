a48d2bf8ecf5a3bb2ebf9d39283ba3e5970474d7
Tom Taylor <ttaylor3.142@gmail.com>
2020-09-01 16:12:49 +0100

    Alpha 1

--- src/mumble-tf2-connect.py
+++ src/mumble-tf2-connect.py
@@ -1,16 +1,17 @@
 import pymumble_py3 as pymumble
+import time
 import re
 import sys
 import xml.etree.ElementTree as xml
 
 connect_regex = r'(?:<p>)?connect\s*(\S*)\s*;\s*password\s*(\S*)(?:</p>)?'
 xml_wrapper = '<?xml version="1.0"?><message>{}</message>'
-_example_connect = "connect eepily.cool:2345; password cool"
+run = True
 
 def recive_message(message, mumble):
     user = mumble.users[message.actor]
     channel = mumble.channels[user['channel_id']]
-    print(user, message, channel)
+    print("input", user, message, channel)
 
     response = "sorry i don't recognise that format"
 
@@ -25,9 +26,14 @@ def recive_message(message, mumble):
                     url += '/' + m.group(2)
                 print("url : ", url)
                 response = '<a href={0}>{0}</a>'.format(url)
+                break
+            else:
+                if text.startswith('!join'):
+                    channel.move_in()
+                    response = 'moving into your channel...'
+                    break
 
-    print(response)
-
+    print("output", response)
     channel.send_text_message(response)
 
 def main():
@@ -38,7 +44,8 @@ def main():
     mumble.callbacks.add_callback('text_received',
                                   lambda m: recive_message(m, mumble))
 
-    mumble.callbacks.reset_callback('text_received')
+    while run:
+        time.sleep(1)
 
 if __name__ == '__main__':
     main()
