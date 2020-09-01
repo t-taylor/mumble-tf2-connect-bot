55af6e09fdb2d7ee95cf6f43d7215bb7c9863a09
Tom Taylor <ttaylor3.142@gmail.com>
2020-09-01 14:33:46 +0100

    Formats connects and sends back link

--- src/mumble-tf2-connect.py
+++ src/mumble-tf2-connect.py
@@ -1,25 +1,32 @@
 import pymumble_py3 as pymumble
+import re
 import sys
+
 
 def recive_message(message, mumble):
     user = mumble.users[message.actor]
     channel = mumble.channels[user['channel_id']]
-    channel.send_text_message(message.message)
+    m = re.match(r'.*connect\s*(\S*)\s*;\s*password\s*(\S*)<*', message.message)
+    response = ''
+    if m is not None:
+        url = 'steam://connect/' + m.group(1)
+        if m.group(2) is not None:
+            url += '/' + m.group(2)
+        response = '<a href={0}>{0}</a>'.format(url)
+    else:
+        response = "sorry i don't recognise that format"
+
+    channel.send_text_message(response)
 
 def main():
     mumble = pymumble.Mumble('localhost', 'tf2-connect')
     mumble.start()
     mumble.is_ready()
     mumble.channels.find_by_name('Root').move_in()
-    channel = mumble.channels.find_by_name('onedotone')
-    channel.send_text_message('test')
-
     mumble.callbacks.add_callback('text_received',
                                   lambda m: recive_message(m, mumble))
 
-    mumble.callbacks.get_callbacks_list()
     mumble.callbacks.reset_callback('text_received')
-
 
 if __name__ == '__main__':
     main()
